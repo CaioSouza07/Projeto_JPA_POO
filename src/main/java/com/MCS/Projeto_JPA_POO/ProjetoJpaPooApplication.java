51e5221b8164702960ee7193b0c3fbb8f51d6b8e
CAIO DE SOUZA <caiosouza@univille.br>
2025-09-17 21:39:19 -0300

    Iniciando projetinho

---
+++ src/main/java/com/MCS/Projeto_JPA_POO/ProjetoJpaPooApplication.java
@@ -0,0 +1,13 @@
+package com.MCS.Projeto_JPA_POO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJpaPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJpaPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MCS/Projeto_JPA_POO/ProjetoJpaPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MCS.Projeto_JPA_POO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJpaPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
